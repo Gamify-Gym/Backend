9a4c8c1f53af30348f552f3cb7f55cd9c97c97a5
leofesteves <estevesleonardo01@gmail.com>
2025-10-16 11:57:49 -0300

    feat: Diet endpoints

--- src/main/java/org/gamify/gym/app/dieta/controller/DietaController.java
+++ src/main/java/org/gamify/gym/app/dieta/controller/DietaController.java
@@ -2,6 +2,8 @@ package org.gamify.gym.app.dieta.controller;
 
 import java.util.List;
 
+import org.gamify.gym.app.dieta.dto.AlimentoDto;
+import org.gamify.gym.app.dieta.model.Alimento;
 import org.gamify.gym.app.dieta.model.Dieta;
 import org.gamify.gym.app.dieta.service.DietaService;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +27,7 @@ public class DietaController {
     }
 
     @GetMapping(value = "/plan", produces = "application/json")
-     public ResponseEntity<?> getDiet(Authentication authentication) {
+    public ResponseEntity<?> getDiet(Authentication authentication) {
         try {
             Jwt jwt = (Jwt) authentication.getPrincipal();
             String email = jwt.getClaimAsString("sub");
@@ -35,26 +38,49 @@ public class DietaController {
                     .body("Error: " + e.getMessage());
         }
      }
-
-     @DeleteMapping(value = "/food")
-     public ResponseEntity<?> delete
- @PostMapping(value = "/food", produces = "application/json")
-
-
+     
      @PostMapping(value = "/plan", produces = "application/json")
-   
-        public ResponseEntity<?> insertDieta(@RequestBody String nomeDieta, Authentication authentication) {
-            try {
-                Jwt jwt = (Jwt) authentication.getPrincipal();
-                String email = jwt.getClaimAsString("sub");
-                dietaService.insertDieta(email, nomeDieta);
-                Dieta dieta = dietaService.insertDieta(email, nomeDieta);
-                 return ResponseEntity.ok(dieta);
+    public ResponseEntity<?> insertDieta(@RequestBody String nomeDieta, Authentication authentication) {
+        try {
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            String email = jwt.getClaimAsString("sub");
+            dietaService.insertDieta(email, nomeDieta);
+            Dieta dieta = dietaService.insertDieta(email, nomeDieta);
+                return ResponseEntity.ok(dieta);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error: " + e.getMessage());
         }
-    }}
+    }
+
+    @DeleteMapping(value = "/food")
+    public ResponseEntity<?> deleteFood(Authentication authentication,Long idAlimento) {
+        try {   
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        String email = jwt.getClaimAsString("sub");
+        dietaService.deleteAlimento(email, idAlimento);
+        return ResponseEntity.ok("Succesfully deleted food");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error: " + e.getMessage());
+        }
+     }
+
+
+    @PostMapping(value = "/food", produces = "application/json")
+    public ResponseEntity<?> insertFood(@RequestBody AlimentoDto dto,Authentication authentication) {
+        try {
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            String email = jwt.getClaimAsString("sub");
+            Alimento food = dietaService.insertAlimentos(email,dto.idDieta() ,dto);
+            return ResponseEntity.ok(food);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error: " + e.getMessage());
+        }
+    }
+
+    }
             
         
 
--- src/main/java/org/gamify/gym/app/dieta/dto/AlimentoDto.java
+++ src/main/java/org/gamify/gym/app/dieta/dto/AlimentoDto.java
@@ -2,7 +2,7 @@ package org.gamify.gym.app.dieta.dto;
 
 public record AlimentoDto(String nome, Double calories, Double fats, Double carbs, Double fibers, Double sodium,
         Double proteins, Double acucarTotal, Double acucarAdicionado, Double gorduraTrans, Double gordurasMonosaturadas,
-        Double gordurasPoliinsaturadas, Double gordurasSaturadas) {
+        Double gordurasPoliinsaturadas, Double gordurasSaturadas,Long idDieta) {
     // Q coiso longo mdsss
 
 }
